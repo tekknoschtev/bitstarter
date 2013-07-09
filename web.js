29c841c222e351b15078087e9f36153e130e951a
Steve Schroeder <tekknoschtev@gmail.com>
2013-07-08 21:08:40 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var text = fs.readFileSync('index.html');
+    
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
